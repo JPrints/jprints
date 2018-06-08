da65c3981a62fdcd36385da15bed52a1937eb81d
Peter West <pjw@repositoryservices,co.uk>
2018-06-08 12:59:06 +0100

    add django stuff to tests

--- core/test_core.py
+++ core/test_core.py
@@ -1,8 +1,23 @@
-from django.test import TestCase
+#from django.test import TestCase
+
+from django.contrib.auth.models import User
+#from django.db import models
+
+#from core.models import Permission
+#from core.models import Role
+#from core.models import Person
 
 # Create your tests here.
 
 def test_one():
         x = "this"
         assert 'h' in x
+
+
+#class PermissionMethodTests(TestCase):
+#    def test_user_admin_role(self):
+#        #p = Permission(name = 'test1') 
+#        #self.assertEqual(p.name, 'test1')
+#        x = "this"
+#        assert 'h' in x
 
